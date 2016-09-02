54855a6f9faa609b864db2d801c45ed1b6e1de1c
TheRealGLH <TheRealGLH@users.noreply.github.com>
2016-09-02 14:04:11 +0200

    Basic level generation and movement

---
+++ Assets/script/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour {
+
+    public float MoveSpeed;
+    public float MoveSpeedX;
+    
+    public GameObject World;
+    public float speedFallof;
+    public float xSpeedFallof;
+    private float XSpeedFallof;
+    private levelGenerator world;
+    
+    private CharacterController characterController;
+    private Vector3 Speed;
+    private float xspeed;
+    private float yspeed;
+    private bool isMovingLeftRight;
+
+    void Start()
+    {
+        world = World.GetComponent<levelGenerator>();       
+        characterController = gameObject.GetComponent<CharacterController>();
+        Speed = new Vector3(0, 0, MoveSpeed);
+    }
+    
+    void FixedUpdate()
+    {
+        if(!MathStuff.IsBetween<float>(xspeed,-speedFallof,speedFallof))
+        {
+            XSpeedFallof += XSpeedFallof;
+            if (xspeed < 0) xspeed += XSpeedFallof;
+            else xspeed -= XSpeedFallof;
+        }
+        else
+        {
+            XSpeedFallof = xSpeedFallof;
+            xspeed = 0;
+            isMovingLeftRight = false;
+        }
+        Speed = new Vector3(xspeed,yspeed,MoveSpeed);
+        characterController.SimpleMove(Speed);
+    }
+
+
+
+    public void MoveRight()
+    {
+        if (!isMovingLeftRight)
+        {
+            xspeed = MoveSpeedX;
+            isMovingLeftRight = true;
+        }
+    }
+
+    public void MoveLeft()
+    {
+        if (!isMovingLeftRight)
+        {
+            xspeed = -MoveSpeedX;
+            isMovingLeftRight = true;
+        }
+    }
+
+    public void Jump()
+    {
+
+    }
+
+}
