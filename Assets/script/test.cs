99e86fff091b17459e88a8cf99cb8787ef29d783
TheRealGLH <TheRealGLH@users.noreply.github.com>
2016-09-02 11:54:10 +0200

    Initial Commit

---
+++ Assets/script/test.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	
+	void Update () {
+
+        this.transform.Translate(0.0003F, 0.0002F, 0.0004F);
+	}
+}
---
+++ Assets/script/world/levelGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class levelGenerator : MonoBehaviour {
+
+
+    [Header("Level pieces")]
+    public GameObject emptyPiece;
+    public List<GameObject> easyPieces;
+    [Header("Settings")]
+    public int timeUntilMedium;
+    public int timeUntilHard;
+    private int currTime;
+
+
+}
